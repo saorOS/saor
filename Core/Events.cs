376aafdccd0f8114f479d604e29451de27453863
jenspto <jenspto@ik.me>
2024-09-07 21:51:09 +0200

    Add Events class.

---
+++ Core/Events.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace saór.Core
+{
+    public static class Events
+    {
+        public static void Neutral()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static string Neutral(string name, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            return name + ": " + message;
+        }
+        public static string Warning(string name, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return name + ": " + message;
+        }
+        public static string Success(string name, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            return name + ": " + message;
+        }
+        public static string FatalError(string name, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            return name + ": " + message;
+        }
+        public static string Error(string name, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            return name + ": " + message; 
+        }
+    }
+}
---
+++ Core/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.System;
+using Cosmos.System.Graphics;
+using Cosmos.System.Graphics.Fonts;
+using Console = System.Console;
+
+namespace saór.Core
+{
+    /// <summary>
+    /// saór entry point. Most objects here are public and static for a direct access from other classes.
+    /// </summary>
+    public class Program : Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Events.Neutral("vga", "Initializing VGA Font...");
+            VGAScreen.SetFont(PCScreenFont.Default.CreateVGAFont(), PCScreenFont.Default.Height);
+            Console.Clear();
+            Events.Success("vga", "VGA Font initialized!");
+
+        }
+        protected override void Run()
+        {
+        }
+    }
+}

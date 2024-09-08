95802ab37c763e63f3aa4d18bcdef8e887939d98
Giovanni Voltan <jenspto@ik.me>
2024-09-08 09:49:22 +0200

    Update Kernel

--- source/Core/Program.cs
+++ source/Core/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Cosmos.System;
 using Cosmos.System.Graphics;
 using Cosmos.System.Graphics.Fonts;
+using System;
 using Console = System.Console;
 
 namespace saór.Core
@@ -12,12 +13,20 @@ namespace saór.Core
     {
         protected override void BeforeRun()
         {
-            Events.Neutral("vga", "Initializing VGA Font...");
-            VGAScreen.SetFont(PCScreenFont.Default.CreateVGAFont(), PCScreenFont.Default.Height);
-            Console.Clear();
-            Events.Success("vga", "VGA Font initialized!");
-
-
+            Console.WriteLine(Events.Neutral("vga", "Initializing VGA Font..."));
+            try
+            {
+                VGAScreen.SetFont(PCScreenFont.Default.CreateVGAFont(), PCScreenFont.Default.Height);
+                Console.Clear();
+                Console.WriteLine(Events.Success("vga", "VGA Font initialized!"));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(Events.FatalError("vga", "Fatal error while setting the VGA Font: " + ex.Message));
+                Console.WriteLine("Press any key to shutdown.");
+                Console.ReadKey();
+                Power.Shutdown();
+            }
         }
         protected override void Run()
         {

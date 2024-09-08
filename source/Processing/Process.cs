654a202985ecc30db7d03ae11521f8a166ee5adc
Giovanni Voltan <jenspto@ik.me>
2024-09-08 09:32:06 +0200

    Add processing

---
+++ source/Processing/Process.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saor.Processing
+{
+    public class Process
+    {
+        public string name, description;
+        public bool running;
+        public Process(string name, string description, bool running = true)
+        {
+            this.name = name;
+            this.description = description;
+            this.running = running;
+        }
+        public void Update()
+        {
+            if (running)
+            {
+                Run();
+            }
+        }
+        protected virtual void Run()
+        {
+
+        }
+    }
+}

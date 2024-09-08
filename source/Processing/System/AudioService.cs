f2498d9c62ffd02eb32aa35f01e438ea2d43ba2d
Giovanni Voltan <jenspto@ik.me>
2024-09-08 15:57:02 +0200

    Create AudioService.cs

---
+++ source/Processing/System/AudioService.cs
@@ -0,0 +1,57 @@
+﻿using Cosmos.HAL.Drivers.Audio;
+using Cosmos.System.Audio.IO;
+using Cosmos.System.Audio;
+using saor.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace saor.Processing.System
+{
+    public class AudioService : Process
+    {
+        public AudioMixer audioMixer;
+        public AC97 audioDriver;
+        public AudioService() : base("audio", "Audio service, used to play sounds in the system.")
+        {
+            try
+            {
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(Events.Error("audio", "Audio not initialized: " + ex.Message));
+                Console.WriteLine(Events.Warning("audio", "Audio disabled."));
+                running = false;
+            }
+        }
+        public void PlayAudio(AudioStream audioStream)
+        {
+            audioMixer.Streams.Add(audioStream);
+        }
+        public void Enable()
+        {
+            Console.WriteLine(Events.Neutral("audio", "Initializing audio..."));
+            audioMixer = new AudioMixer();
+            var audioStream = MemoryAudioStream.FromWave(Resources.bootAudio);
+            audioDriver = AC97.Initialize(bufferSize: 4096);
+            audioMixer.Streams.Add(audioStream);
+
+            var audioManager = new AudioManager()
+            {
+                Stream = audioMixer,
+                Output = audioDriver
+            };
+            audioManager.Enable();
+            Console.WriteLine(Events.Success("audio", "Audio initialized and boot sound played."));
+            running = true;
+        }
+        public void Disable()
+        {
+            audioDriver.Disable();
+            running = false;
+        }
+    }
+}
